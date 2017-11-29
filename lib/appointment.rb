2361f179d83eb9515994714f72ddf4e8026463d6
Jacqueline Lee <anginblu@gmail.com>
2017-11-29 08:25:38 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -10,14 +10,14 @@ class Appointment
   def patient=(patient)
     raise AssociationTypeMismatchError, "Patient class is expected" if !patient.is_a?(Patient)
     unless self.patient == patient
-    @patient = patient
-    doctor.patients << self.patient
+      @patient = patient
+      doctor.patients << self.patient
     end
   end
 
   def doctor=(doctor)
     raise AssociationTypeMismatchError, "Artist class is expected" if !artist.is_a?(Artist)
     @doctor = doctor unless self.doctor == doctor
-    end
+    
   end
 end
