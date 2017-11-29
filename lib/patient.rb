c9387c95fc5a3a7f5161f65ef332c929d4e51f67
Jacqueline Lee <anginblu@gmail.com>
2017-11-29 08:26:02 +0000

    automatically backed up by learn

---
+++ lib/patient.rb
@@ -0,0 +1,23 @@
+class Appointment
+  attr_accessor :name, :patient, :doctor
+
+  def initialize(name, doctor)
+    @name = name
+    @doctor = doctor
+    doctor.appointments << self
+  end
+
+  def patient=(patient)
+    raise AssociationTypeMismatchError, "Patient class is expected" if !patient.is_a?(Patient)
+    unless self.patient == patient
+      @patient = patient
+      doctor.patients << self.patient
+    end
+  end
+
+  def doctor=(doctor)
+    raise AssociationTypeMismatchError, "Artist class is expected" if !artist.is_a?(Artist)
+    @doctor = doctor unless self.doctor == doctor
+  end
+
+end
